3e6754f4399c2b837ca79635bcfe229b1d4268ca
lozevBogdan <lozev.bogdan@abv.bg>
2022-06-01 14:54:30 +0300

    Pathfinder with BaseEntity.

---
+++ pathFinder/src/main/java/bg/softuni/pathfinder/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package bg.softuni.pathfinder.model;
+
+import org.hibernate.annotations.Generated;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public BaseEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+}
