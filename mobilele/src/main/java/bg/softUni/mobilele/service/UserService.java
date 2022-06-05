e0cb3f704b2489b8ac7e0ed937e04dc31272d282
lozev <lozev.bogdan@abv.bg>
2022-06-05 14:08:57 +0300

    CurrentUser and methods for logout and login.

--- mobilele/src/main/java/bg/softUni/mobilele/service/UserService.java
+++ mobilele/src/main/java/bg/softUni/mobilele/service/UserService.java
@@ -3,6 +3,7 @@ package bg.softUni.mobilele.service;
 import bg.softUni.mobilele.model.dto.UserLoginDto;
 import bg.softUni.mobilele.model.entity.UserEntity;
 import bg.softUni.mobilele.repository.UserRepository;
+import bg.softUni.mobilele.user.CurrentUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,11 @@ public class UserService {
     private Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     private UserRepository userRepository;
+    private CurrentUser currentUser;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, CurrentUser currentUser) {
         this.userRepository = userRepository;
+        this.currentUser = currentUser;
     }
 
 
@@ -31,11 +34,26 @@ public class UserService {
             return false;
         }
 
+        boolean success = userOpt.get().getPassword().equals(userLoginDto.getPassword());
 
+        if(success){
+            login(userOpt.get());
+        }else {
+            logout();
+        }
+        return success;
 
+    }
 
-        return true;
+    private void login(UserEntity userEntity){
+        currentUser
+                .setLoggedIn(true)
+                .setName(userEntity.getFirstName() + userEntity.getLastName());
 
+    }
+
+    private void logout(){
+        currentUser.clear();
     }
 
 
