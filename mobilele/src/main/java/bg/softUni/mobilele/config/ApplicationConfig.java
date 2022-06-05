97e1242fc48c5a4aa89f574088726ec6fdada854
lozev <lozev.bogdan@abv.bg>
2022-06-05 15:38:55 +0300

    Adding configuration class with passwordEncoder.

---
+++ mobilele/src/main/java/bg/softUni/mobilele/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package bg.softUni.mobilele.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@Configuration
+public class ApplicationConfig {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new Pbkdf2PasswordEncoder();
+    }
+
+}
