ba38f40df0b1d450e1f3c77ca24f188cbc5a40fc
lozev <lozev.bogdan@abv.bg>
2022-06-11 18:14:21 +0300

    Project DB structure!

---
+++ pathFinder/src/main/java/bg/softuni/pathfinder/model/CategoryEntity.java
@@ -0,0 +1,37 @@
+package bg.softuni.pathfinder.model;
+
+import bg.softuni.pathfinder.model.enums.CategoryEnum;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="categories")
+public class CategoryEntity extends BaseEntity {
+
+    @Enumerated
+    private CategoryEnum name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public CategoryEnum getName() {
+        return name;
+    }
+
+    public CategoryEntity setName(CategoryEnum name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public CategoryEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+}
---
+++ pathFinder/src/main/java/bg/softuni/pathfinder/model/enums/CategoryEnum.java
@@ -0,0 +1,5 @@
+package bg.softuni.pathfinder.model.enums;
+
+public enum CategoryEnum {
+    PEDESTRIAN, BICYCLE, MOTORCYCLE, CAR
+}
