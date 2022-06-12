766aac80c6777c18f7f43caa59b36c35e7d21fcf
lozev <lozev.bogdan@abv.bg>
2022-06-12 21:07:54 +0300

    User Registration validations!

--- pathFinder/src/main/java/bg/softuni/pathfinder/web/AuthController.java
+++ pathFinder/src/main/java/bg/softuni/pathfinder/web/AuthController.java
@@ -2,21 +2,59 @@ package bg.softuni.pathfinder.web;
 
 import bg.softuni.pathfinder.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthController {
 
+    // with @ModelAttribute method annotation we attach to Model UserRegisterDto,
+    // which we can access in everywhere in templates.
+    // Its important to put the NAME OF ATTRIBUTE, AFTER ANNOTATION If the method is void !!!!!!!!!!!!!
+    //@ModelAttribute("NAMEOFTTRIBUTE") in other way,
+    // the nameof ModelAttribute will become the name of returned class!!!!!!!
 
-    @GetMapping("/register")
-    public String register(){
-        return "register";
+    @ModelAttribute("userRegistrationDto")
+    public void initForm(Model model){
+        model.addAttribute("userRegistrationDto",new UserRegistrationDto());
     }
 
+
+    @GetMapping("/register")
+
+        public String register(){
+
+        return "register";
+    }
+  //  redirectAttributes.addFlashAttribute attach attribute to Model !!!!!
     @PostMapping("/register")
-    public String register(UserRegistrationDto userRegistrationDto){
-        System.out.println(userRegistrationDto);
+    public String register(@Valid UserRegistrationDto userRegistrationDto,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes){
+
+        if(bindingResult.hasErrors()){
+            redirectAttributes.
+                    addFlashAttribute(
+                    "org.springframework.validation.BindingResult.userRegistrationDto",
+                            bindingResult);
+            redirectAttributes.
+                    addFlashAttribute("userRegistrationDto", userRegistrationDto);
+
+            return "redirect:/register";
+        }
+
+        // check if passwords match
+        //check if username/email is used
+
+
+        // insert in DB
+
         return "redirect:/login";
     }
 
