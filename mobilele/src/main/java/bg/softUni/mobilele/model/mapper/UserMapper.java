5b72ef5c749398d9e38f2499879baf38f216daf3
lozev <lozev.bogdan@abv.bg>
2022-06-13 21:39:28 +0300

    Adding a useful comments to UserMapper interface

--- mobilele/src/main/java/bg/softUni/mobilele/model/mapper/UserMapper.java
+++ mobilele/src/main/java/bg/softUni/mobilele/model/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package bg.softUni.mobilele.model.mapper;
 
-public class UserMapper {
+
+import org.mapstruct.Mapper;
+
+// With this annotation we define UserMapper int will be mapper,
+// componentModel = "spring" means that generated mapper is a Spring bean and
+// can be retrieved via @Autowire
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+
 }
