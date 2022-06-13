e777ad6b1cc8865d6f8f6bb8fb92adbc89b66aea
lozev <lozev.bogdan@abv.bg>
2022-06-13 21:46:56 +0300

    worked UserMapper

--- mobilele/src/main/java/bg/softUni/mobilele/model/mapper/UserMapper.java
+++ mobilele/src/main/java/bg/softUni/mobilele/model/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package bg.softUni.mobilele.model.mapper;
 
 
+import bg.softUni.mobilele.model.dto.UserRegisterDto;
+import bg.softUni.mobilele.model.entity.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 // With this annotation we define UserMapper int will be mapper,
 // componentModel = "spring" means that generated mapper is a Spring bean and
@@ -10,5 +13,10 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    // with  @Mapping(target = "active", constant = "true"), we set for property "active"
+    // value "true"
+
+    @Mapping(target = "active", constant = "true")
+    UserEntity userDtoToUserEntity(UserRegisterDto userRegisterDto);
 
 }
