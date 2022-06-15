f0933ad8a959caf03acd7844dad6fa0b4e77317a
lozevBogdan <lozev.bogdan@abv.bg>
2022-06-15 17:28:02 +0300

    ModelDto

--- mobilele/src/main/java/bg/softUni/mobilele/model/dto/ModelDto.java
+++ mobilele/src/main/java/bg/softUni/mobilele/model/dto/ModelDto.java
@@ -4,4 +4,25 @@ import java.util.List;
 
 public class ModelDto {
 
+    private long id;
+
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public ModelDto setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ModelDto setName(String name) {
+        this.name = name;
+        return this;
+    }
 }
