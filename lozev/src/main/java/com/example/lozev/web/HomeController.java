730f59583ebb887dd89e094a5a08578eea0cf942
lozev <lozev.bogdan@abv.bg>
2022-06-19 19:33:36 +0300

    Coming soon home page

---
+++ lozev/src/main/java/com/example/lozev/web/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.lozev.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String homePage(){
+        return "index";
+    }
+
+}
