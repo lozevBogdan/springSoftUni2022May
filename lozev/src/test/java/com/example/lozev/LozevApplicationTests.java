873c399950f618c405f6e248ca041a1b5fdd4227
lozev <lozev.bogdan@abv.bg>
2022-06-19 18:12:16 +0300

    My first personal site .

---
+++ lozev/src/test/java/com/example/lozev/LozevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lozev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LozevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
