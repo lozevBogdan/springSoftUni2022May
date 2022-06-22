7fcba9db7882a828cf9980dde8babaedb415b94f
lozev <lozev.bogdan@abv.bg>
2022-06-22 17:21:00 +0300

    initialization of categories

--- coffieShopExmaPrep/src/main/java/com/example/coffieshopexmaprep/service/CategoryService.java
+++ coffieShopExmaPrep/src/main/java/com/example/coffieshopexmaprep/service/CategoryService.java
@@ -1,7 +1,45 @@
 package com.example.coffieshopexmaprep.service;
 
+import com.example.coffieshopexmaprep.entity.CategoryEntity;
+import com.example.coffieshopexmaprep.entity.enums.CategoryEnum;
+import com.example.coffieshopexmaprep.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void initCategories() {
+
+        if(this.categoryRepository.count() != 0){
+            return;
+        }
+
+        Arrays.stream(CategoryEnum.values()).forEach(c->
+                {
+                    CategoryEntity newCategory = new CategoryEntity();
+                    newCategory.setName(c);
+                    newCategory.setNeededTime(
+                    switch (c){
+                        case CAKE -> 10;
+                        case DRINK -> 1;
+                        case COFFEE -> 2;
+                        case OTHER -> 5;
+                    });
+
+                    this.categoryRepository.save(newCategory);
+                    System.out.println();
+
+                });
+
+
+    }
 }
