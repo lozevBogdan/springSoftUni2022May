306a2353b461c908dfa90af3cdd33274acd0e7d9
lozev <lozev.bogdan@abv.bg>
2022-06-22 12:03:57 +0300

    application, static and templates !

---
+++ coffieShopExmaPrep/src/main/java/com/example/coffieshopexmaprep/CoffieShopExmaPrepApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffieshopexmaprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffieShopExmaPrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffieShopExmaPrepApplication.class, args);
+    }
+
+}
---
+++ coffieShopExmaPrep/src/test/java/com/example/coffieshopexmaprep/CoffieShopExmaPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffieshopexmaprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffieShopExmaPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
