c72c832153251a2a35bae4bfd144496d5a552d70
lozev <lozev.bogdan@abv.bg>
2022-06-23 11:12:37 +0300

    Basic structure of battleships project

---
+++ BattleShips/src/main/java/com/example/battleShips/init/InitDb.java
@@ -0,0 +1,14 @@
+package com.example.battleShips.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitDb implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
