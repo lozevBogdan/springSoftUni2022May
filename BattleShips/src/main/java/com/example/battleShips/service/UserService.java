c6626c4b62b921aee1db19d02035c449dfb5a5f9
lozev <lozev.bogdan@abv.bg>
2022-06-23 13:06:27 +0300

    login

--- BattleShips/src/main/java/com/example/battleShips/service/UserService.java
+++ BattleShips/src/main/java/com/example/battleShips/service/UserService.java
@@ -1,20 +1,26 @@
 package com.example.battleShips.service;
 
+import com.example.battleShips.currUser.CurrentUser;
+import com.example.battleShips.dtos.UserLoginDto;
 import com.example.battleShips.dtos.UserRegisterDto;
 import com.example.battleShips.entity.UserEntity;
 import com.example.battleShips.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final CurrentUser currentUser;
 
-    public UserService(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserService(UserRepository userRepository, ModelMapper modelMapper, CurrentUser currentUser) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.currentUser = currentUser;
     }
 
     public UserEntity register(UserRegisterDto userRegisterDto) {
@@ -47,4 +53,22 @@ public class UserService {
         return (password.equals(confirmPassword));
     }
 
+
+    public Optional<UserEntity> checkForExistingUsernameWithPassword(String username, String password) {
+
+        Optional<UserEntity> byUsernameAndPassword =
+                this.userRepository.findByUsernameAndPassword(username, password);
+
+        return byUsernameAndPassword;
+    }
+
+    public void loginUser(UserLoginDto userLoginDto){
+
+        UserEntity newUser = this.userRepository.findByUsernameAndPassword(userLoginDto.getUsername(),
+                userLoginDto.getPassword()).get();
+
+        this.currentUser.setUsername(newUser.getUsername()).setId(newUser.getId());
+
+
+    }
 }
--- BattleShips/src/main/java/com/example/battleShips/web/UserController.java
+++ BattleShips/src/main/java/com/example/battleShips/web/UserController.java
@@ -2,8 +2,10 @@ package com.example.battleShips.web;
 
 import com.example.battleShips.dtos.UserLoginDto;
 import com.example.battleShips.dtos.UserRegisterDto;
+import com.example.battleShips.entity.UserEntity;
 import com.example.battleShips.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
@@ -22,6 +25,11 @@ public class UserController {
     @ModelAttribute
     public UserRegisterDto userRegisterDto(){
         return new UserRegisterDto();
+    }
+
+    @ModelAttribute
+    public UserLoginDto userLoginDto(){
+        return new UserLoginDto();
     }
 
     public UserController(UserService userService) {
@@ -69,7 +77,13 @@ public class UserController {
 
 
     @GetMapping("/login")
-    public String loginPage(){
+    public String loginPage(Model model){
+
+
+        if(!model.containsAttribute("existingUser")){
+            model.addAttribute("existingUser",true);
+
+        }
         return "login";
     }
 
@@ -78,13 +92,21 @@ public class UserController {
     public String register(@Valid UserLoginDto userLoginDto, BindingResult bindingResult,
                            RedirectAttributes redirectAttributes){
 
+        Optional<UserEntity> existingUser =
+                this.userService.checkForExistingUsernameWithPassword(userLoginDto().getUsername(),
+                userLoginDto.getPassword());
+
+        System.out.println();
+
         if (bindingResult.hasErrors()) {
             redirectAttributes.addFlashAttribute("userLoginDto", userLoginDto);
             redirectAttributes.addFlashAttribute(
                     "org.springframework.validation.BindingResult.userLoginDto", bindingResult);
-
+            redirectAttributes.addFlashAttribute("existingUser", existingUser);
             return "redirect:/users/login";
         }
+
+        this.userService.loginUser(userLoginDto);
         return "redirect:/";
     }
 
