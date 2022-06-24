1c4ae986b1969a02ef6bab18ea58e21ace0c3bdd
lozev <lozev.bogdan@abv.bg>
2022-06-24 14:06:06 +0300

    skeleton of project - exam Shopping List

---
+++ ShoppingList/src/main/java/com/example/examspringfundskeleton/init/InitDb.java
@@ -0,0 +1,16 @@
+package com.example.examspringfundskeleton.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.xml.stream.events.Comment;
+
+@Component
+public class InitDb implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
