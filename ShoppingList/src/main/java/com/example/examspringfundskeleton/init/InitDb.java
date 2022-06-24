39a49b6a24afb02ae37117327f9c7f582116790d
lozev <lozev.bogdan@abv.bg>
2022-06-24 18:01:14 +0300

    Initilalize a Categories in DB

--- ShoppingList/src/main/java/com/example/examspringfundskeleton/init/InitDb.java
+++ ShoppingList/src/main/java/com/example/examspringfundskeleton/init/InitDb.java
@@ -1,16 +1,39 @@
 package com.example.examspringfundskeleton.init;
 
+import com.example.examspringfundskeleton.entity.CategoryEntity;
+import com.example.examspringfundskeleton.entity.CategoryNameEnum;
+import com.example.examspringfundskeleton.repositories.CategoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import javax.xml.stream.events.Comment;
+import java.util.Arrays;
 
 @Component
 public class InitDb implements CommandLineRunner {
 
+    private final CategoryRepository categoryRepository;
+
+    public InitDb(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
 
     @Override
     public void run(String... args) throws Exception {
+
+        if (this.categoryRepository.count() == 0){
+
+            Arrays.stream(CategoryNameEnum.values()).forEach(c->{
+
+                CategoryEntity category = new CategoryEntity();
+                category.
+                        setName(c).
+                        setDescription(c.name().toString());
+
+                this.categoryRepository.save(category);
+
+            });
+        }
 
     }
 }
